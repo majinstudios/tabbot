2de7744f88c4053b8b58ad8a350067a49090b983
Irene Sanz <irenesanznieto@gmail.com>
2016-08-17 18:16:09 +0200

    Added v1.0 functionality.

--- Software/RoboGuide/Arduino/src/RoboGuide.ino
+++ Software/RoboGuide/Arduino/src/RoboGuide.ino
@@ -3,62 +3,26 @@
 
 Tabbot tabbot;
 //Path init
-// Movement path [6]={
-//   {forwards,  0.3,"", 0},
-//   {0, 0, "Hi! How are you?", 2},
-//   {0, 0, "I am Tabbot, your guide for today's tour", 0},
-//   {right, 0.1, "", 0},
-//   {forwards, 0.3, "", 0},
-//   {left, 0.5, "", 0}
+// Movement path []={
+//   {forwards,  1,"", 0},
+//   {stop, 1, "", 0}
 // };
-
-// Movement path [10]={
-//   {forwards,  1.15,"", 0},
-//   {left, 0.22, "", 0},
-//   {forwards, 0.9, "", 0},
-//   {backwards, 0.9, "", 0},
-//   {right, 0.25, "", 0},
-//   {forwards, 1.8, "", 0},
-//   {left, 0.25, "", 0},
-//   {forwards, 0.8, "", 0},
-//   {right, 0.25, "", 0},
-//   {forwards, 1.3, "", 0}
-// };
-
-Movement path []={
-  {forwards,  1,"", 0},
-  {stop, 1, "", 0}
-};
-// Movement_encoder path_encoder []={
-//   {47,-47,"",0},
-//   {44,44,"",0},
-//   {-43, 43,"",0},
-//   {-79, -79 ,"",0}
-// };
-
-Movement_encoder path_encoder []={{104 , -104,"",0},{-3 , -3,"",0},{-3 , -3,"",0},{168 , -168,"",0},{39 , 39,"",0},{2 , 2,"",0},{73 , -73,"",0},{-1 , -1,"",0},{-1 , -1,"",0},{80 , -80,"",0},{-1 , -1,"",0},{0 , 0,"",0},{98 , -98,"",0}};
+Movement_encoder path_encoder []={{0, 0, "I am Tabbot, your robotic guide. Please, follow me.", 3},{102 , -102,"",0},{-25 , -25,"",0},{0, 0, "If you look to your left, you can see the kitchen.", 5},{16 , 16,"",0},{157 , -157,"",0},{-24 , -24,"",0},{-2 , -2,"",0},{0, 0, "Please follow me into the living room.", 0},{50 , -50,"",0},{45 , -45,"",0},{18 , 18,"",0},{153 , -153,"",0},{45 , 45,"",0}};
 
 int btPinState=14;
 
 void setup() {
-
   Serial.begin(9600);
   tabbot.init();
   pinMode(btPinState,INPUT);
-
 }
-
 
 void loop() {
   double blinkSpeed = pulseIn(btPinState, HIGH,1000000);    //If no pulse in 1 seconds return 0
-  String incoming="";
 
-  tabbot.move_path(path_encoder, sizeof(path_encoder)/sizeof(path_encoder[0]));
-  // tabbot.get_saved_path();
-
-  if(blinkSpeed==0){    //Connected
+  if(blinkSpeed==0){    // Connected via BT
     Serial.flush();
-    // tabbot.move_control();
+    tabbot.move_path(path_encoder, sizeof(path_encoder)/sizeof(path_encoder[0])); // Move the robot following the path defined in the variable path_encoder
     Serial.print("\n");
     Serial.println("Disconnect");
     Serial.flush();
@@ -71,10 +35,8 @@ ISR(PCINT2_vect) {
   char result2 = tabbot.r2.process();
   if (result) {
     result == DIR_CW ? tabbot.degrees_left++ : tabbot.degrees_left--;
-    //Serial.println("Encoder 1: "+String(degrees_left));
   }
   if (result2) {
     result2 == DIR_CW ? tabbot.degrees_right++ : tabbot.degrees_right--;
-    //Serial.println("Encoder 2: "+String(degrees_right));
   }
 }
